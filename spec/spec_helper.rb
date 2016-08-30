fde9f193f853be5f24039572f52793cc3ee3d345
Carlos Ribeiro <mail@carlosribeiro.me>
2016-08-30 11:47:32 -0300

    Configure dotenv and vcr

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,18 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'cognitive_vision'
+require 'dotenv'
+require 'pry'
+require 'vcr'
+
+Dotenv.load
+
+CognitiveVision.configure do |config|
+  config.key = ENV['VISION_API_KEY']
+end
+
+VCR.configure do |c|
+  c.filter_sensitive_data('<API_KEY>') { CognitiveVision.configuration.key }
+  c.cassette_library_dir = 'spec/cassettes'
+  c.hook_into :webmock
+  c.configure_rspec_metadata!
+end
