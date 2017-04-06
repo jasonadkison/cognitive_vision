d3f063ff2f33ab669cbbd8ec31f9a111f9fc8426
Jason Adkison <jadkison@gmail.com>
2017-04-06 12:26:09 -0500

    updates api endpoint and bumps to version 0.4.1 (#2)
    * updates api endpoint and bumps to version 0.4.1
    
    * fixes endpoints within cassettes; fixes SimpleCov usage
    
    * upload codeclimate results after_success

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,13 +1,6 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'simplecov'
-require 'codeclimate-test-reporter'
-SimpleCov.start do
-  formatter SimpleCov::Formatter::MultiFormatter.new([
-    SimpleCov::Formatter::HTMLFormatter,
-    CodeClimate::TestReporter::Formatter
-  ])
-end
-CodeClimate::TestReporter.start
+SimpleCov.start
 
 require 'cognitive_vision'
 require 'dotenv'
